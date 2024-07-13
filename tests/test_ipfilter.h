3cf56ca56c326b1fe082b7307ad87173bdfc361a
Aleksei Tihonov <atichonov@yandex.ru>
2024-07-14 00:42:40 +0300

    add some tests

--- tests/test_ipfilter.h
+++ tests/test_ipfilter.h
@@ -2,9 +2,13 @@
 #include <ipfilter.h>
 #include <gtest/gtest.h>
 
+#include <memory>
+
 class Test_IpFilter : public ::testing::Test
 {
 protected:
     void SetUp();
     void TearDown();
+
+    std::shared_ptr<IpFilter> m_pFilter;
 };
